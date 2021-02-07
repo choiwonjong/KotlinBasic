d054e59589eb79178a093714a6bad90e0279d991
CWJ <cozycoxy@naver.com>
2021-02-07 16:43:32 +0900

    버튼추가 이벤트처리

--- app/src/main/java/com/tjoeun/android/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/tjoeun/android/kotlinbasic/MainActivity.kt
@@ -2,12 +2,18 @@ package com.tjoeun.android.kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sendBtn.
+        sendBtn.setOnClickListener {
+ //    Ctrl + I 로 이줄을 주석
+            Log.d("메인화면", "전송버튼이 눌림")
+
+        }
     }
 }
